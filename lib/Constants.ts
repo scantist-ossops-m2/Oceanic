da45a2229237d817ddb51759d3019250645d4063
Donovan Daniels <hewwo@yiff.rocks>
2024-06-21 14:09:37 -0500

    Add `DURABLE` and `CONSUMABLE` sku types & `AVAILABLE` sku flag

--- lib/Constants.ts
+++ lib/Constants.ts
@@ -1270,11 +1270,14 @@ export enum AttachmentFlags {
 }
 
 export enum SKUTypes {
+    DURABLE            = 2,
+    CONSUMABLE         = 3,
     SUBSCRIPTION       = 5,
     SUBSCRIPTION_GROUP = 6,
 }
 
 export enum SKUFlags {
+    AVAILABLE          = 1 << 2,
     GUILD_SUBSCRIPTION = 1 << 7,
     USER_SUBSCRIPTION  = 1 << 8,
 }
